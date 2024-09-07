391a95bc020bb3845341b94023746f5b5da5699b
k1759h <sonmond421@gmail.com>
2024-09-07 08:21:41 +0000

    課題３

--- routes/web.php
+++ routes/web.php
@@ -1,18 +1,11 @@
 <?php
-
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\Admin\ProfileController;
 
-/*
-|--------------------------------------------------------------------------
-| Web Routes
-|--------------------------------------------------------------------------
-|
-| Here is where you can register web routes for your application. These
-| routes are loaded by the RouteServiceProvider within a group which
-| contains the "web" middleware group. Now create something great!
-|
-*/
 
-Route::get('/', function () {
-    return view('welcome');
+Route::prefix('admin')->group(function () {
+
+  Route::get('/admin/profile/create', [ProfileController::class,'add']);
+  Route::get('/admin/profile/edit', [ProfileController::class,'edit']);
 });
+
