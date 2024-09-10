9cca7ac39092b7fd48bc64d41a0b5591688168ba
k1759h <sonmond421@gmail.com>
2024-09-10 17:57:45 +0000

    課題３・４

---
+++ resources/views/admin/profile/create.blade.php
@@ -0,0 +1,9 @@
+<!DOCTYPE html>
+<html>
+    <head>
+        <title>Create Profile</title>
+    </head>
+    <body>
+        <h1>プロフィール作成ページ</h1>
+    </body>
+</html>
---
+++ resources/views/admin/profile/edit.blade.php
@@ -0,0 +1,9 @@
+<!DOCTYPE>
+<html>
+    <head>
+        <title>Edit Profile</title>
+    </head>
+    <body>
+        <h1>プロフィール編集ページ</h1>
+    </body>
+</html>
