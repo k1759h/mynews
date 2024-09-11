b3176744760feeefae09dfe9a993a74f7b9cf7a3
k1759h <sonmond421@gmail.com>
2024-09-11 16:55:11 +0000

    半角に修正しました

--- app/Http/Middleware/TrustProxies.php
+++ app/Http/Middleware/TrustProxies.php
@@ -12,7 +12,7 @@ class TrustProxies extends Middleware
      *
      * @var array<int, string>|string|null
      */
-    protected $proxies = "＊";
+    protected $proxies = "*";
 
     /**
      * The headers that should be used to detect proxies.
