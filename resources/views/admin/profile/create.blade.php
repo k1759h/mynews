87bf56ad6592ffc1bc761cf8c4f1a0c18d4b4b90
k1759h <sonmond421@gmail.com>
2024-09-28 15:04:11 +0000

    13 課題

--- resources/views/admin/profile/create.blade.php
+++ resources/views/admin/profile/create.blade.php
@@ -12,14 +12,39 @@
     <body>
         <h1>プロフィール作成ページ</h1>
         
-         @section('content')
+    @section('content')
         <div class="container">
           <div class="row">
              <div class="col-md-8 mx-auto">
                 <h2>プロフィール新規作成</h2>
+                <form action="{{ action('Admin/ProfileController@create') }}" method="post">
+                    @csrf
+                    <div class="form-group">
+                        <label for="name">氏名</label>
+                        <input type="text" class="form-control" name="name" value="{{ old('name') }}">
+                    </div>
+                     <div class="form-group">
+                        <label for="gender">性別</label>
+                        <select class="form-control" name="gender">
+                            <option value="male">男性</option>
+                            <option value="female">女性</option>
+                            <option value="other">その他</option>
+                        </select>
+                    </div>
+                    <div class="form-group">
+                        <label for="hobby"z>趣味</label>
+                        <input type="text" class="form-control" name="hobby" varue="{{ old('hobby') }}">
+                    </div>
+                    <div class="form-group">
+                        <label for="introduction">自己紹介欄</label>
+                        <textarea class="form-control" name="introduction" rows="5">{{ old('introduction') }}</textarea>
+                    </div>
+                    <input type="submit" class="btn btn-primary" varue="作成">
+                </form>
              </div>
           </div>
         </div>
-        @endsection
+    @endsection    
+        
     </body>
 </html>
--- routes/web.php
+++ routes/web.php
@@ -4,8 +4,10 @@ use App\Http\Controllers\Admin\NewsController;
 use App\Http\Controllers\Admin\ProfileController;
 
 
-Route::controller(NewsController::class)->group(function() {
-    Route::get('admin/news/create', 'add')->middleware('auth');
+Route::controller(NewsController::class)->prefix('admin')->name('admin.')->middleware('auth')->group(function () {
+    Route::get('news/create', 'add')->name('news.add');
+    Route::post('news/create', 'create')->name('news.create');
+
 });
 
 
@@ -20,6 +22,9 @@ Route::middleware(['auth'])->group(function() {
   Route::get('/admin/profile/create' ,[ProfileController::class,'create'])->name('admin.profile.create');
   Route::get('/admin/profile/edit' ,[ProfileController::class,'edit'])->name('admin.profile.edit');
 });
+
+Route::post('admin/profile/create',[ProfileController::class, 'create']);
+Route::post('admin/profile/edit',[ProfileController::class, 'update']);
 
 Auth::routes();
 
